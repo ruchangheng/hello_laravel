77ae8db35ec15febe09005f26724c598ff0212b2
ruchangheng <624813409@qq.com>
2020-01-19 07:43:30 +0000

    say something

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel-ruchangheng
                 </div>
 
                 <div class="links">
